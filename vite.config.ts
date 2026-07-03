7b391c6dc34787e6cda4e663e4b473cbd0e6f9b9
namimon-mahjong <122211846+namimon-mahjong@users.noreply.github.com>
2026-07-03 20:24:35 +0900

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/mahjong_solitier/",
+  base: "/mahjong-solitaire/",
   resolve: {
     alias: {
       assert: "assert/",
